e209522790569632dacad6ffb1fba835d7c06114
Jennifer Zajac <zajac.je@northeastern.edu>
2023-03-19 01:46:00 -0400

    fix dying

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -5,30 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    private bool dead = false;
+    private readonly float lowestPlatformY = -1;
+
     private void Update() {
-        if (transform.position.y < -15) {
+        // So we don't die on each frame during delay
+        if (transform.position.y < lowestPlatformY && !dead) {
             Die();
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Enemy Body")) {
+            // Make player invisible
+            GetComponent<MeshRenderer>().enabled = false;
+
+            // Stop other objects pushing the player
+            GetComponent<Rigidbody>().isKinematic = true;
+
+            // Disable player movement
+            GetComponent<PlayerMovement>().enabled = false;
+
+            // Alternatively, Destroy(gameObject).
+            // But this destroys the player and PlayerLife script, so we cannot reset the level.
+
             Die();
         }
     }
 
     void Die() {
-        // Make player invisible
-        GetComponent<MeshRenderer>().enabled = false;
+        
 
-        // Stop other objects pushing the player
-        GetComponent<Rigidbody>().isKinematic = true;
-
-        // Disable player movement
-        GetComponent<PlayerMovement>().enabled = false;
-
-        // Alternatively, Destroy(gameObject).
-        // But this destroys the player and PlayerLife script, so we cannot reset the level.
+        
+        dead = true;
 
         // Reload the level... with a delay using Invoke
         Invoke(nameof(ReloadCurrentLevel), 1.3f);
