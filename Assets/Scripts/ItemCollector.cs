cb6c9a6c3cf8705fec328e8096278862bc78c277
Jennifer Zajac <zajac.je@northeastern.edu>
2023-03-19 02:49:55 -0400

    UI for coins

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
     int coinsCollected = 0;
 
+    [SerializeField] TMP_Text coinsText;
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Coin")) {
             Destroy(other.gameObject);
             coinsCollected += 1;
-            Debug.Log("Coins: " + coinsCollected);
+            coinsText.text = "Coins: " + coinsCollected;
         }
     }
 }
